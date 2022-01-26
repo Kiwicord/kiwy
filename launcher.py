0495d9d81d9a118b756300c8f56bf17641d213d4
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-01-26 17:59:35 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,5 +1,31 @@
-from lib.bot import bot
+import discord
+from discord.ext import commands
+from os import getenv
+from dotenv import load_dotenv
+from db import *
 
-VERSION = '0.0.2'
+from commands.balance import Balance
+from commands.deposit import Deposit
 
-bot.run(VERSION)
+PREFIX = '.'
+
+load_dotenv()
+
+client = commands.Bot(command_prefix=PREFIX)
+
+@client.event
+async def on_ready():
+    print('------')
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+async def setup():
+    await client.wait_until_ready()
+    client.add_cog(Balance(client))
+    client.add_cog(Deposit(client))
+
+client.loop.create_task(setup())
+
+client.run(getenv('TOKEN'))
