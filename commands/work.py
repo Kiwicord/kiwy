e77430c73f32ab21496859d564e5a703e6d01fe6
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-02-06 17:09:08 +0100

    Work command redesign

--- commands/work.py
+++ commands/work.py
@@ -11,52 +11,76 @@ class Work(commands.Cog):
     @commands.group(invoke_without_command=True)
     async def work(self, ctx, option=None):
         jobs = await get_jobs()
-        current_job = await get_current_job(ctx.author.id)
+        current_job = bank.find_one({'_id': ctx.author.id})
         job = current_job['job']
 
         joblist = []
         for job in jobs:
             joblist.append(job['_id'])
         
-        try:             
-            if option is None:
-                if job == 'unemployed':
-                    await ctx.send('Du bist momentan arbeitslos!\nFinde einen job mit -work list')
-                user = bank.find_one({'_id': ctx.author.id})
-                user_job = user['job']
-                worktable = await get_job(user_job)
-                income = worktable['income']
-                job_name = worktable['name']
-                await update_wallet(ctx.author.id, income)
-                work_embed = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Gute Arbeit!', description=f'Du hast als **{job_name}** gearbeitet und hast **{income:,}**🥝 verdient')
-                await ctx.send(embed=work_embed)
-                
-            embed = discord.Embed(title='<a:kc_bewegendeszeichenlmao:934397592178135121> Jobs', color=0x77dd77)
+        # try:             
+        if option is None:
+            if current_job['job'] == 'unemployed':
+                embed_unemployed = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Arbeitslos!', description='Du bist momentan arbeitslos. Finde einen Job mit **.work list**.')
+                await ctx.send(embed=embed_unemployed)
+                return
             
-            if option is not None:  
-                # if option == 'list':
-                #     jobs = await get_jobs()
-                #     for job in jobs:
-                #         embed.add_field(name=f'{job["name"]}', value=f'Beschreibung: {job["description"]}\nStündliches Einkommen: **{job["income"]:,}**🥝\nID: `{job["_id"]}`', inline=False)
-                #     embed.set_footer(text='Um zu arbeiten, führe .work (ID vom Job) aus')
-                #     await ctx.send(embed=embed)
-                
-                if option in joblist:
-                    final_job = await get_job(option)
-                    bank.update_one({'_id': ctx.author.id,}, {'$set': {'job': option}})
-                    new_work_embed = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Willkommen auf der neuen Arbeit!', description=f'Du arbeitest nun als **{final_job["name"]}**! Dein Einkommen beträgt nun **{final_job["income"]:,}**🥝')
-                    await ctx.send(embed=new_work_embed)
-        except:
-            pass
+            user = bank.find_one({'_id': ctx.author.id})
+            user_job = user['job']
+            worktable = await get_job(user_job)
+            income = worktable['income']
+            job_name = worktable['name']
+            await update_wallet(ctx.author.id, income)
+            work_embed = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Gute Arbeit!', description=f'Du hast als **{job_name}** gearbeitet und hast **{income:,}**🥝 verdient')
+            await ctx.send(embed=work_embed)
+            
+        embed = discord.Embed(title='<a:kc_bewegendeszeichenlmao:934397592178135121> Jobs', color=0x77dd77)
+        
+        if option is not None:  
+            # if option == 'list':
+            #     jobs = await get_jobs()
+            #     for job in jobs:
+            #         embed.add_field(name=f'{job["name"]}', value=f'Beschreibung: {job["description"]}\nStündliches Einkommen: **{job["income"]:,}**🥝\nID: `{job["_id"]}`', inline=False)
+            #     embed.set_footer(text='Um zu arbeiten, führe .work (ID vom Job) aus')
+            #     await ctx.send(embed=embed)
+            
+            if option in joblist:
+                final_job = await get_job(option)
+                bank.update_one({'_id': ctx.author.id,}, {'$set': {'job': option}})
+                new_work_embed = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Willkommen auf der neuen Arbeit!', description=f'Du arbeitest nun als **{final_job["name"]}**! Dein Einkommen beträgt nun **{final_job["income"]:,}**🥝')
+                await ctx.send(embed=new_work_embed)
+            elif option not in joblist:
+                embed_not_found = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Dieser Beruf wurde nicht gefunden!')
+                embed_not_found.set_footer('Tipp: benutze .work list um einen Job zu finden.')
+                await ctx.send(embed=embed_not_found)
+                return
+        # except:
+        #     pass
     
     @work.command(name='list')
     async def list(self, ctx):
         embed = discord.Embed(title='<a:kc_bewegendeszeichenlmao:934397592178135121> Jobs', color=0x77dd77)
         jobs = await get_jobs()
         for job in jobs:
-            embed.add_field(name=f'{job["name"]}', value=f'Beschreibung: {job["description"]}\nStündliches Einkommen: **{job["income"]:,}**🥝\nID: `{job["_id"]}`', inline=False)
+            embed.add_field(name=f'» {job["name"]}', value=f'● Beschreibung: {job["description"]}\n● Stündliches Einkommen: **{job["income"]:,}**🥝\n● ID: `{job["_id"]}`', inline=False)
         embed.set_footer(text='Um zu arbeiten, führe .work (ID vom Job) aus')
         await ctx.send(embed=embed)
+
+    @work.command()
+    async def resign(self, ctx):
+        current_job = await get_current_job(ctx.author.id)
+        job = current_job['job']
+        if job == 'unemployed':
+            embed_unemployed = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Du bist arbeitslos und kannst nicht von deiner Arbeit zurücktreten!')
+            await ctx.send(embed=embed_unemployed)
+            return
+        
+        if job != 'unemployed':
+            bank.update_one({'_id': ctx.author.id,}, {'$set': {'job': 'unemployed'}})
+            embed = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Ciao!', description='Du bist nun von deinem Job zurückgetreten.\nDu kannst erst in **1 Stunde** einen neuen Job finden.')
+            await ctx.send(embed=embed)
+            return
+
                 
 def setup(client):
     client.add_cog(Work(client))
