6fc118e83d3825e3c33fee426aacc1f175d6c558
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-02-11 15:00:50 +0100

    kick cmd message reply

--- commands/kick.py
+++ commands/kick.py
@@ -6,11 +6,11 @@ class Kick(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def ban(self, ctx, member: discord.Member, *, reason='Kein Grund angegeben'):
-        embed1 = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Gebannt!', description=f'Der Member {member.mention} wurde gebannt.')
-        embed2 = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Gebannt!', description=f'Du wurdest von **{ctx.guild.name}** gebannt.\n<:kc_punkt:924409447147786261> **{reason}**')
-        await member.send(embed=embed2)
-        await member.ban(reason=reason)
+    async def kick(self, ctx, member: discord.Member, *, reason='Kein Grund angegeben'):
+        embed1 = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Gekickt!', description=f'Der Member {member.mention} wurde gekickt.')
+        embed2 = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Gekickt!', description=f'Du wurdest von **{ctx.guild.name}** gekickt.\n<:kc_punkt:924409447147786261> **{reason}**')
+        await member.reply(embed=embed2, mention_author=False)
+        await member.kick(reason=reason)
         await ctx.send(embed=embed1)
 
 def setup(client):
