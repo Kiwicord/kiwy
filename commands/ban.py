e0e1e4eec4d2ed142192bb3320364d0b117f92db
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-02-11 14:58:14 +0100

    Ban cmd

--- commands/ban.py
+++ commands/ban.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 from db import *
-import random
 
 class Ban(commands.Cog):
     def __init__(self, client):
@@ -9,10 +8,10 @@ class Ban(commands.Cog):
 
     @commands.command()
     async def ban(self, ctx, member: discord.Member, *, reason='Kein Grund angegeben'):
+        embed1 = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Gebannt!', description=f'Der Member {member.mention} wurde gebannt.')
         embed2 = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Gebannt!', description=f'Du wurdest von **{ctx.guild.name}** gebannt.\n<:kc_punkt:924409447147786261> **{reason}**')
         await member.send(embed=embed2)
         await member.ban(reason=reason)
-        embed1 = discord.Embed(color=0x77dd77, title='<a:kc_bewegendeszeichenlmao:934397592178135121> Gebannt!', description=f'Der Member {member.mention} wurde gebannt.')
         await ctx.send(embed=embed1)
 
 def setup(client):
