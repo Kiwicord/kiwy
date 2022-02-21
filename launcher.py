ec8140b8271fa268d0f5a791f12e7b7f49ae5dd1
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-02-21 17:03:14 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -22,7 +22,7 @@ async def load(ctx, extension):
 
 @client.command()
 async def unload(ctx, extension):
-    if ctx.author.id == 733403498766401554
+    if ctx.author.id == 733403498766401554:
         client.unload_extension(f'commands.{extension}')
 
 for filename in os.listdir("./commands"):
