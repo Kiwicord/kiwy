4160ce66292cb71bc07cee08cdc70964f0996980
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-02-22 20:01:45 +0100

    do shop for members ls

--- commands/shop.py
+++ commands/shop.py
@@ -32,23 +32,40 @@ class Shop(commands.Cog):
             return
 
     @commands.command()
-    async def inv(self, ctx):
+    async def inv(self, ctx, member: discord.Member=None):
         await open_profile(ctx.author.id)
         inv = await get_inv(ctx.author.id)
         embed = discord.Embed(color=0x77dd77, title=f"{Kiwicord.EXCLAMATION} {ctx.author}'s Inventar")
 
-        if len(inv) == 0:
-            embed.title = f'{Kiwicord.EXCLAMATION} Du hast momentan keine Items!'
-            embed.set_footer(text='Besuche den Shop indem du .shop ausführst um Items zu kaufen.')
-            await ctx.reply(embed=embed, mention_author=False)
-            return
+        if member is None:
+            if len(inv) == 0:
+                embed.title = f'{Kiwicord.EXCLAMATION} Du hast momentan keine Items!'
+                embed.set_footer(text='Besuche den Shop indem du .shop ausführst um Items zu kaufen.')
+                await ctx.reply(embed=embed, mention_author=False)
+                return
 
-        for item in await get_shop_items():
-            amount = inv.count(item["_id"])
-            if amount > 0:
-                embed.add_field(name=f"{Kiwicord.DOT} {item['name']}", value=f"ID: `{item['_id']}`\nStückzahl: `{amount}`", inline=False)
-        embed.set_footer(text='Erfahre mehr über ein Item indem du .item <ID> benutzt.')
-        await ctx.reply(embed=embed, mention_author=False)
+            for item in await get_shop_items():
+                amount = inv.count(item["_id"])
+                if amount > 0:
+                    embed.add_field(name=f"{Kiwicord.DOT} {item['name']}", value=f"ID: `{item['_id']}`\nStückzahl: `{amount}`", inline=False)
+            embed.set_footer(text='Erfahre mehr über ein Item indem du .item <ID> benutzt.')
+            await ctx.reply(embed=embed, mention_author=False)
+        else:
+            await open_profile(member.id)
+            embed = discord.Embed(color=0x77dd77, title=f"{Kiwicord.EXCLAMATION} {member}'s Inventar")
+            inv_member = await get_inv(member.id)
+            if len(inv_member) == 0:
+                embed.title = f'{Kiwicord.EXCLAMATION} Der User besitzt momentan keine Items!'
+                embed.set_footer(text='Besuche den Shop indem du .shop ausführst um Items zu kaufen.')
+                await ctx.reply(embed=embed, mention_author=False)
+                return
+
+            for item in await get_shop_items():
+                amount = inv_member.count(item["_id"])
+                if amount > 0:
+                    embed.add_field(name=f"{Kiwicord.DOT} {item['name']}", value=f"ID: `{item['_id']}`\nStückzahl: `{amount}`", inline=False)
+            embed.set_footer(text='Erfahre mehr über ein Item indem du .item <ID> benutzt.')
+            await ctx.reply(embed=embed, mention_author=False)  
     
     @commands.command()
     async def shop(self, ctx):
