61b176318db008d7d28d4e198df1b685d8e8091b
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-02-23 16:29:03 +0100

    amount all

--- commands/slots.py
+++ commands/slots.py
@@ -2,15 +2,16 @@ import discord
 from discord.ext import commands
 from db import *
 import random
+from emojis import Kiwicord
 
-EMOJIS = ['<:kiwi_disguised:941275906377396225>', '<:kiwi_hearts:941275654685597696>', '<:kiwi_sunglasses:941275906197049355>']
+EMOJIS = ['<:kiwi_disguised:941275906377396225>', '<:kiwi_hearts:941275654685597696>', '<:kiwi_sunglasses:941275906197049355>','<:kiwi_disguised:941275906377396225>', '<:kiwi_hearts:941275654685597696>', '<:kiwi_sunglasses:941275906197049355>','<:kiwi_disguised:941275906377396225>', '<:kiwi_hearts:941275654685597696>', '<:kiwi_sunglasses:941275906197049355>', ]
 
 class Slots(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.command(aliases=['Slots', 'SLOTS'])
-    async def slots(self, ctx, amt : int):
+    async def slots(self, ctx, amt : int=None):
         await open_profile(ctx.author.id)
         amt = int(amt)
         wallet_amount = await get_wallet(ctx.author.id)
@@ -19,6 +20,13 @@ class Slots(commands.Cog):
         chance2 = random.choice(EMOJIS)
         chance3 = random.choice(EMOJIS)
 
+        if amt is None:
+            error = discord.Embed(color=0x77dd77, title=f'{Kiwicord.EXCLAMATION} Bitte gib den Betrag an!')
+            await ctx.send(embed=error)
+            return
+        
+        if amt == 'all' or 'max':
+            amt = wallet_amount
 
         if wallet_amount < amt:
             not_enough_money = discord.Embed(color=0xff6961, title='<a:7732exclamationred:939902470111522856> Nicht genügend Geld!')
