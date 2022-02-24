df2a1bc18d1e8b2e9767262d738da15d4d88fd78
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-02-24 18:58:33 +0100

    Update knast.py

--- commands/knast.py
+++ commands/knast.py
@@ -10,22 +10,28 @@ class Knast(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def knastadd(self, ctx, member: discord.Member):
-        role = discord.utils.get(ctx.guild.roles, id=946455955418284034)
-        role_standard = discord.utils.get(ctx.guild.roles, id=899574259532300292)
-        embed = discord.Embed(color=0x77dd77, title=f'{Kiwicord.EXCLAMATION} {member} wurde in den Knast geschickt!')
-        await ctx.reply(embed=embed, mention_author=False)
-        await member.add_roles(role)
-        await member.remove_roles(role_standard)
+        if ctx.guild.id == 845217487419211776:
+            role = discord.utils.get(ctx.guild.roles, id=946455955418284034)
+            role_standard = discord.utils.get(ctx.guild.roles, id=899574259532300292)
+            embed = discord.Embed(color=0x77dd77, title=f'{Kiwicord.EXCLAMATION} {member} wurde in den Knast geschickt!')
+            await ctx.reply(embed=embed, mention_author=False)
+            await member.add_roles(role)
+            await member.remove_roles(role_standard)
+        else:
+            raise commands.CommandNotFound
     
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def knastremove(self, ctx, member: discord.Member):
-        role = discord.utils.get(ctx.guild.roles, id=946455955418284034)
-        role_standard = discord.utils.get(ctx.guild.roles, id=899574259532300292)
-        embed = discord.Embed(color=0x77dd77, title=f'{Kiwicord.EXCLAMATION} {member} wurde aus dem Knast geschickt!')
-        await ctx.reply(embed=embed, mention_author=False)
-        await member.add_roles(role_standard)
-        await member.remove_roles(role)
+        if ctx.guild.id == 845217487419211776:
+            role = discord.utils.get(ctx.guild.roles, id=946455955418284034)
+            role_standard = discord.utils.get(ctx.guild.roles, id=899574259532300292)
+            embed = discord.Embed(color=0x77dd77, title=f'{Kiwicord.EXCLAMATION} {member} wurde aus dem Knast geschickt!')
+            await ctx.reply(embed=embed, mention_author=False)
+            await member.add_roles(role_standard)
+            await member.remove_roles(role)
+        else:
+            raise commands.CommandNotFound
 
 def setup(client):
     client.add_cog(Knast(client))
