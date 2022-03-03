5aafd408e44811efb280654c9a4920d5363ca5a0
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-03-03 15:24:16 +0100

    add new emojis

--- emojis.py
+++ emojis.py
@@ -2,3 +2,4 @@ class Kiwicord:
     EXCLAMATION = '<a:kc_bewegendeszeichenlmao:934397592178135121>'
     DOT = '<:kc_punkt:924409447147786261>'
     TADA = '<a:tada:945426772625866802>'
+    LOADING = '<a:Ripple1s200px1:948595717289697300>'
