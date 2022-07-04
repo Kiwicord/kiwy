254e73a2226e4c4ced33ccecd0caac8401d8e114
lauchrens <104091795+lauchrens@users.noreply.github.com>
2022-07-04 11:21:14 +0200

    sus

---
+++ commands/jason.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Jason(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def jason(self, ctx):
+        embed =  discord.Embed(title='', description='Jason Stinkt sehr dolle...')
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(Jason(client))
